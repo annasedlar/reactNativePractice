95e2c7859e7f9336a015987be6b9614fa1b61dda
Anna Sedlar <annasedlar@lawn-128-61-46-177.lawn.gatech.edu>
2017-11-14 17:59:25 -0500

    button and textInput

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
+
+  learnMore() {
+    Alert.alert('Oye como va, mi ritmo')
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello!!</Text>
-        <Text>I am Anna</Text>
+        <Text style={styles.name}>I am Anna</Text>
         <Text>I make apps</Text>
+        <Button onPress={this.learnMore} title="Learn More" color="#841584" style={styles.button} />
+        <TextInput style={{height:40}} placeholder="Type here" onChangeText={(text) => this.setState({text})} />
       </View>
     );
   }
@@ -22,4 +34,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name: {
+    fontSize: 20,
+    margin: 30,
+    borderColor: 'purple',
+    borderWidth: 2,
+    padding: 10
+  },
+  button: {
+    color: "#841584", 
+    padding: 10,
+    margin: 20,
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: "#841584",
+  }
 });
